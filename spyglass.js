44afe4bf9ed4689360b6c088635d607d6e73239a
Martial <marq.d.jefferson@gmail.com>
2014-05-29 11:52:02 -0400

    init

---
+++ spyglass.js
@@ -0,0 +1,54 @@
+(function(g){
+  /**
+  * Represents the Spyglass global module
+  * @module Spyglass
+  * @description Analytics library for PixieTV
+  * @author PixieTV <pixie@samsungaccelerator.com>
+  * @version 1.0
+  */
+  
+  function Spyglass(){
+    
+  }
+  
+  /**
+  * @name up
+  * @function
+  * @description initializes Spyglass and enables tracking of events
+  */
+  Spyglass.up = function(){
+    
+  }
+  
+  /**
+  * @name down
+  * @function
+  * @description disables Spyglass and tracking of events
+  */
+  Spyglass.down = function(){
+    
+  }
+  
+  /**
+  * @name identify
+  * @function
+  * @description identifies and associates tracked events with user
+  */
+  Spyglass.identify = function(){
+    
+  }
+  
+  /**
+  * @name event
+  * @function
+  * @description tracks an event
+  * @param {string} event Event name
+  * @param {object} metadata Event metadata
+  */
+  Spyglass.event = function(){
+    
+  }
+  
+  g.Spyglass = g.sg = Spyglass;
+  
+})(window);
